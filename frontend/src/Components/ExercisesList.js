a978f6d7934eaf1674384b831badd1d44fb5e672
najm09 <fnajm09@gmail.com>
2022-07-14 01:15:52 +0530

    Added : delete request

--- frontend/src/Components/ExercisesList.js
+++ frontend/src/Components/ExercisesList.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { styled } from '@mui/material/styles';
+import { CircularProgress } from '@mui/material';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell, { tableCellClasses } from '@mui/material/TableCell';
@@ -7,14 +8,17 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { Button } from '@mui/material';
 import { Typography } from '@mui/material';
 import { useState, useEffect } from 'react';
 import './ExerciseList.css'
-import ErrorPage from './Error'
 
 const ExercisesList = () => {
 
   const [userData, setUserData] = useState([]);
+  const [shouldUpdateUserData, setShouldUpdateUserData] = useState(false)
+  const [deleteMessage, setDeleteMessage] = useState("")
+  const [loader, setLoader] = useState(false)
 
 
   const StyledTableCell = styled(TableCell)(({ theme }) => ({
@@ -37,33 +41,64 @@ const ExercisesList = () => {
     },
   }));
 
-  const handleEdit = () => {
 
+  const handleDelete =  async(id) => {
+    setShouldUpdateUserData(false);
+    try{
+      const response =  await axios.delete(`http://localhost:5000/exercises/${id}`);
+      if(response?.status == '200'){
+        setDeleteMessage(response?.data);
+        setShouldUpdateUserData(true);
+        console.log(deleteMessage)
+      }else setDeleteMessage(response?.data);
+    }
+    catch(error){
+      setDeleteMessage(error)
+    }
   }
 
   useEffect(() => {
     const getUserData = async () => {
-      const { data: response = [] } = await axios.get('http://localhost:5000/exercises');
-      setUserData(response);
+      try{
+        setLoader(true);
+        const { data: response = [] } = await axios.get('http://localhost:5000/exercises');
+        setUserData(response);
+        if(response === '200') setLoader(false)
+        else setLoader(false)
+      }
+      catch(error){
+        console.log("Error : ", error)
+      }
     }
     getUserData();
-  }, []);
+  }, [shouldUpdateUserData]);
 
   return userData.length ? (
     <div className='header'>
       <div>
-      <Typography center variant = 'h4'>Logged Exercises Lists </Typography>
-      &nbsp;
+        <Typography center variant='h4'>Logged Exercises Lists </Typography>
+        &nbsp;
       </div>
+      <Typography center variant='h5' className = "success-message" >{deleteMessage} </Typography>
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 700 }} aria-label="customized table">
           <TableHead>
             <TableRow>
-              <StyledTableCell>Username</StyledTableCell>
-              <StyledTableCell align="right">Description</StyledTableCell>
-              <StyledTableCell align="right">Duration&nbsp;</StyledTableCell>
-              <StyledTableCell align="right">Date&nbsp;</StyledTableCell>
-              <StyledTableCell align="right">Actions&nbsp;</StyledTableCell>
+              <StyledTableCell>
+                <Typography variant='h5'>Username</Typography>
+              </StyledTableCell>
+              <StyledTableCell align="right">
+                <Typography variant='h5'>Description</Typography>
+              </StyledTableCell>
+              <StyledTableCell align="right">
+                <Typography variant='h5'>Duration</Typography>
+              </StyledTableCell>
+              <StyledTableCell align="right">
+                <Typography variant='h5'>Date</Typography>
+              </StyledTableCell>
+              <StyledTableCell align="right">
+                <Typography variant='h5'>Actions</Typography>
+              </StyledTableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -72,15 +107,18 @@ const ExercisesList = () => {
                 <StyledTableCell component="th" scope="row">{row.username}</StyledTableCell>
                 <StyledTableCell align="right" >{row.description}</StyledTableCell>
                 <StyledTableCell align="right" >{row.duration}</StyledTableCell>
-                <StyledTableCell align="right" >{row.date.substring(0,10)}</StyledTableCell>
-                <StyledTableCell align="right"><a >Edit</a>|<a >Delete</a></StyledTableCell>
+                <StyledTableCell align="right" >{row.date.substring(0, 10)}</StyledTableCell>
+                <StyledTableCell align="right">
+                  <Button color="success">Edit</Button>|
+                  <Button color="error" onClick={() => handleDelete(row._id)}>Delete</Button>
+                </StyledTableCell>
               </StyledTableRow>
             ))}
           </TableBody>
         </Table>
       </TableContainer>
     </div>
-  ) : <ErrorPage/>
+  ) : loader ? <CircularProgress className='loader'/> : <div className='loader'>No data available this time</div>
 }
 
 export default ExercisesList
